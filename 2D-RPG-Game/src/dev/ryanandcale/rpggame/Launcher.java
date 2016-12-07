d542afd789bfab3d726bb80540a2b657497e19c1
Cale Sweeney <cale.sweeney@gmail.com>
2016-12-06 16:27:35 -0800

    Added Launcher.java and Display.java and
    Added Launcher.java to start the game. 
    Added Display.java to initiate the JFrame window.

---
+++ 2D-RPG-Game/src/dev/ryanandcale/rpggame/Launcher.java
@@ -0,0 +1,7 @@
+package dev.ryanandcale.rpggame;
+
+public class Launcher {
+
+	public static void main(String[] args){
+	
+}
---
+++ 2D-RPG-Game/src/dev/ryanandcale/rpggame/display/Display.java
@@ -0,0 +1,21 @@
+package dev.ryanandcale.rpggame.display;
+
+import javax.swing.JFrame;
+
+public class Display {
+	
+	private JFrame frame;
+	
+	private String title;
+	private int width, height;
+	
+	public Display(String title, int width, int height){
+		this.title = title;
+		this.width = width;
+		this.height = height;
+		
+		createDisplay();
+	}
+
+
+}
