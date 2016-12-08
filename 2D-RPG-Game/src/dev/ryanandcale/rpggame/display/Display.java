c75d7d47f4bee9c1a2ec0085416795761a4353c9
Cale Sweeney <cale.sweeney@gmail.com>
2016-12-08 00:06:11 -0800

    Updated Display.java
    Updated Display.java with JFrame and Canvas dimensions. Added a getCanvas method.

--- 2D-RPG-Game/src/dev/ryanandcale/rpggame/display/Display.java
+++ 2D-RPG-Game/src/dev/ryanandcale/rpggame/display/Display.java
@@ -1,10 +1,14 @@
 package dev.ryanandcale.rpggame.display;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Display {
 	
 	private JFrame frame;
+	private Canvas canvas;
 	
 	private String title;
 	private int width, height;
@@ -17,5 +21,25 @@ public class Display {
 		createDisplay();
 	}
 
+	private void createDisplay(){
+		frame = new JFrame(title);
+		frame.setSize(width, height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		canvas = new Canvas();
+		canvas.setPreferredSize(new Dimension(width, height));
+		canvas.setMaximumSize(new Dimension(width, height));
+		canvas.setMinimumSize(new Dimension(width, height));
+		
+		frame.add(canvas);
+		frame.pack();  //resize the window slightly so we can see the canvas entirely
+	}
+	
+	public Canvas getCanvas(){
+		return canvas;
+	}
 
 }
